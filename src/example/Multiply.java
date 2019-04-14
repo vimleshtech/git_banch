8df8411522fa2daaf67737b6395719adfa3b9fdd
vimleshtech <35063663+vimleshtech@users.noreply.github.com>
2019-04-14 18:01:12 +0530

    test

---
+++ src/example/Multiply.java
@@ -0,0 +1,33 @@
+package example;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=1;
+		int y=2;
+		int z=3;
+		
+		int a =x*y*z;
+		System.out.println(a);
+		
+		
+		int  b;
+		a =550;
+		b =67;
+		//show greater no
+		if(a>b)
+		{
+			System.out.println("a is greater");
+		}
+		else
+		{
+			System.out.println("b is greater ");
+		}
+		
+		
+	}
+
+}
+
